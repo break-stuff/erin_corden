b333e0d7b7165b9283469d932e142409320f7f35
break-stuff <burton@breakstuff.com>
2020-12-16 22:43:37 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/scripts/main.js',
+    output: {
+        filename: 'scripts.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, '.'),
+        watchContentBase: true,
+        compress: true,
+        inline: true,
+        hot: true,
+        port: 9000,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /.(scss|sass)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                    'postcss-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+        }),
+    ],
+};
